166034866341c79b4adca69543bb9ba643e27663
unknownpgr <unknownpgr@gmail.com>
2021-10-06 14:06:47 +0000

    Fix error

--- src/init.sh
+++ src/init.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-sed -i 's/__REDIRECT_TO__/'$REDIRECT_TO'/g' /etc/nginx/conf.d/default.conf
-cat /etc/nginx/conf.d/default.conf
-# cat /secret/htpasswd
+sed -i 's*__REDIRECT_TO__*'$REDIRECT_TO'*g' /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
